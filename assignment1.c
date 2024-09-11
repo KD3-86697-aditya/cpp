96f2bcecb04f8f8c68989196606f133a2278671f
unknown <dhumaladitya77@gmail.com>
2024-09-11 15:09:22 +0530

    1 uploaded

--- assignment1.c
+++ assignment1.c
@@ -4,47 +4,71 @@ KD3 batch
 Roll no - 86697
 */
 
-#include<stdio.h>
+#include <stdio.h>
 
-struct Date
-{
+// Structure to hold the date
+struct Date {
     int day;
     int month;
     int year;
-
 };
 
-void initDate(struct Date * ptr)
-{
-  ptr->day=9;
-  ptr->month=9;
-  ptr->year=2024;
-
-  printf("date is initialized to : %d/%d/%d",ptr->day,ptr->month,ptr->year);
+// Function to initialize the date to a default value
+void initDate(struct Date *ptr) {
+    ptr->day = 9;
+    ptr->month = 9;
+    ptr->year = 2024;
+    printf("Date initialized to: %d/%d/%d\n", ptr->day, ptr->month, ptr->year);
 }
 
-void acceptDateOnConsole(struct Date * ptr)
-{
-    printf("Enter the day ");
-    scanf("%d",&ptr->day);
-    printf("Enter the month ");
-    scanf("%d",&ptr->month);
-    printf("Enter the year ");
-    scanf("%d",&ptr->year);
+// Function to accept date details from the console
+void acceptDateFromConsole(struct Date *ptr) {
+    printf("Enter the day: ");
+    scanf("%d", &ptr->day);
+    printf("Enter the month: ");
+    scanf("%d", &ptr->month);
+    printf("Enter the year: ");
+    scanf("%d", &ptr->year);
 }
 
-void printDateOnConsole(struct Date *ptr)
-{
-    printf("date is - %d/%d/%d",ptr->day,ptr->month,ptr->year);
-
-
+// Function to print the date on the console
+void printDateOnConsole(struct Date *ptr) {
+    printf("Date is: %d/%d/%d\n", ptr->day, ptr->month, ptr->year);
 }
 
-int main()
-{
-    struct Date d1;
-    // initDate(&d1);
-    acceptDateOnConsole(&d1);
-   printDateOnConsole(&d1);
+// Main menu-driven program
+int main() {
+    struct Date d1;  // Declare a Date object
+    int choice;
 
+    do {
+        // Display the menu options
+        printf("\n*** Date Menu ***\n");
+        printf("1. Initialize Date (Default Values)\n");
+        printf("2. Accept Date from Console\n");
+        printf("3. Print Date on Console\n");
+        printf("4. Exit\n");
+        printf("Enter your choice (1-4): ");
+        scanf("%d", &choice);
+
+        // Perform actions based on the user choice
+        switch (choice) {
+            case 1:
+                initDate(&d1);
+                break;
+            case 2:
+                acceptDateFromConsole(&d1);
+                break;
+            case 3:
+                printDateOnConsole(&d1);
+                break;
+            case 4:
+                printf("Exiting...\n");
+                break;
+            default:
+                printf("Invalid choice! Please try again.\n");
+        }
+    } while (choice != 4);
+
+    return 0;
 }
